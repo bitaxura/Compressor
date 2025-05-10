2dc009a4e6dcf6a81f7b6f4c300b77f312eed71a
Daksh <jdaksh1322@gmail.com>
2025-05-09 23:47:04 -0400

    actually for realsies fixed the headers for helper files in the main

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@
 #include <chrono>
 #include <thread>
 #include <mutex>
-#include <huffman.h>
-#include <utils.h>
+#include "huffman.h"
+#include "utils.h"
 using namespace std;
 
 void build_frequency_table_parralel_helper(const vector<uint8_t>& data, int start, int end, unordered_map<uint8_t, int>& dict) { 
